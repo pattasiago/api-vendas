2709b263e4f37bf3d08fa82d3b27437a6d75e7a4
Iago Franco <pattasiago@gmail.com>
2022-07-10 19:41:34 -0300

    Create Product Service

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,9 +1,8 @@
 import { response, Router } from 'express';
+import productsRouter from '@modules/products/routes/products.routes';
 
 const routes = Router();
 
-routes.get('/', (request, response) => {
-  response.json({ message: 'Hello Dev!' });
-});
+routes.use('/products', productsRouter);
 
 export default routes;
